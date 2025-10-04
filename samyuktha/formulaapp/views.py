07443c1c441ee413e891290611d620af781f5edc
samyukthas261207 <samyukthas261207@gmail.com>
2025-10-04 08:16:15 +0530

    Success

---
+++ samyuktha/formulaapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+def calculate_bmi(request):
+    bmi=None
+    catagory=None
+    height=None
+    weight=None
+    if request.method=="POST":
+        height=float(request.POST.get("height"))
+        weight=float(request.POST.get("weight"))
+        bmi=weight/((height/100)**2)
+        print(f"Weight: {weight}kg \nHeight: {height}cm \nBMI: {bmi}")
+    return render(request, "formulaapp/math.html",{"bmi": bmi, "height": height, "weight": weight})
+
+# Create your views here.
